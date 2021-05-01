69e94f076b6ae584d4d0b9684fc43a588f4ef8e9
Wolk9 <66083478+Wolk9@users.noreply.github.com>
2021-05-01 12:59:30 +0200

    Begin

---
+++ index.js
@@ -0,0 +1,5 @@
+var person = prompt("Wat is je naam?", "typ hier je naam");
+
+if (person != null) {
+  document.getElementById("AskName").innerHTML; = "Hello " + person + "! How are you today?";
+}
