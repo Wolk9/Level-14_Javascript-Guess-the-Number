c81f232a8c509237fa3ca63b864e3d44c8a77f70
Wolk9 <66083478+Wolk9@users.noreply.github.com>
2021-05-03 17:41:16 +0200

    Verder puzzelen

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-var person = prompt("Wat is je naam?", "typ hier je naam");
+function getName() {
+  if (YourName != null) {
+    document.getElementById("YourName").value;
+    var input = document.getElementById("YourName");
+    input.addEventListener("keyup", function (event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("NameButton").click();
+      }
+    });
+    document.getElementById("demo").innerHTML =
+      "Hello " + YourName.value + "! How are you today?";
+  }
+}
 
-if (person != null) {
-  document.getElementById("AskName").innerHTML; = "Hello " + person + "! How are you today?";
+function getNumber() {
+  document.getElementById("game").innerHTML =
+    "<input type='text' placeholder='1 to 20' id=YourNumber><button onclick='ChalengeNumber()'>guess</button>";
+  if (YourNumber != null) {
+  }
+}
+
+var NumberToGuess = 8;
+
+function ChalengeNumber() {
+  if (YourNumber.value != NumberToGuess) {
+    document.getElementById("game").innerHTML =
+      "The number " + YourNumber.value + " is not correct, try again";
+  } else {
+    document.getElementById("game").innerHTML =
+      "The number " + YourNumber.value + " is correct!";
+  }
 }
